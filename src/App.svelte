a467990608facd4422da0f712b4e765a6c4d12cd
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-05 17:54:17 +0530

    knitting video

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<main>
+	<h1>Slow TV</h1>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/DalFiTRXs0Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</main>
+
+<style>
+</style>
